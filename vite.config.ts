69c4d0d2068ec0e38b1eddbbd01fdef9e9008392
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2026-04-17 15:24:02 +0700

    feat(package): update package.json and vite.config.ts for improvements
    * Change keywords to focus on 'vite'
    * Update exports to include main entry points
    * Add new scripts for bundle visualization and storybook build
    * Refactor vite.config.ts for improved path imports and structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
-import path, { resolve } from 'path'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+import path, { resolve } from 'node:path';
 
 export default defineConfig(() => {
   return {
     resolve: {
       alias: {
-        '@local': path.resolve(__dirname, './src')
-      }
+        '@local': path.resolve(__dirname, './src'),
+      },
     },
     plugins: [
-      tsconfigPaths(),
       dts({
         include: ['src/'],
         exclude: [],
         rollupTypes: true,
         insertTypesEntry: true,
-        tsconfigPath: './tsconfig.json'
-      })
-    ],
+        tsconfigPath: './tsconfig.json',
+      }),
+    ].filter(Boolean),
     publicDir: false,
     build: {
       sourcemap: true,
@@ -27,17 +26,17 @@ export default defineConfig(() => {
       rootDir: './src',
       lib: {
         entry: {
-          ['index']: resolve(__dirname, 'src/index.ts')
+          index: resolve(__dirname, 'src/index.ts'),
         },
         formats: ['es', 'cjs'],
-        fileName: (format, name) => `${name}.${format}.js`
+        fileName: (format, name) => `${name}.${format}.js`,
       },
       rollupOptions: {
         external: ['fs', 'path', 'os', 'sharp'],
         output: {
-          globals: {}
-        }
-      }
-    }
-  }
-})
+          globals: {},
+        },
+      },
+    },
+  };
+});
